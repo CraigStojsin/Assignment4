9fb2ba930b8eb156eac46a1ed7297c5b16f940e1
CraigStojsin <stojsin@outlook.com>
2015-04-08 22:28:34 -0400

    count by two

---
+++ countByTwo.m
@@ -0,0 +1,78 @@
+//
+//  countByTwo.m
+//  TheFourthAssignment
+//
+//  Created by Craig Stojsin on 2015-04-08.
+//  Copyright (c) 2015 Codes By Craig. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+
+
+
+
+
+
+//int countByTwo() {
+//    static int currentCount = 0;
+//
+//    currentCount +=2;
+//
+//    return currentCount;
+//}
+int main(int argc, const char * argv[]) {
+    //    @autoreleasepool {
+    //
+    //        NSLog(@"%d", countByTwo());
+    //        NSLog(@"%d", countByTwo());
+    //        NSLog(@"%d", countByTwo());
+    //    }
+    //
+    return 0;
+}
+
+/*int getRandomInteger(int minimum, int maximum){
+ return arc4random_uniform((maximum - minimum  ) + 1) + minimum;
+ }
+ */
+
+/*NSString *getRandomMake(NSArray *makes){
+ int maximum = (int)[makes count];
+ int randomIndex = arc4random_uniform(maximum);
+ return makes[randomIndex];
+ */
+
+//Declaration
+/*NSString *getRandomMake(NSArray *);
+ NSArray *makes = @[@"Honda", @"Ford", @"Nissan", @"Porshe"];
+ NSLog(@"Selected a %@", getRandomMake(makes));*/
+
+//implementation
+
+//        NSString *getRandomMake(NSArray *makes) {
+//            int maximum = (int)[makes count];
+//            int randomIndex = arc4random_uniform(maximum);
+//            return makes[randomIndex];
+//        }
+//
+
+/*
+ int randomNumber = getRandomInteger(-10, 10);
+ NSLog(@"Selected a random number between -10 and 10: %d", randomNumber);
+ */
+//int main(int argc, const char * argv[]) {
+//  @autoreleasepool {
+
+
+// Static function declaration
+//static int getRandomInteger(int, int);
+
+// Static function implementation
+//static int getRandomInteger(int minimum, int maximum) {
+//    return arc4random_uniform((maximum - minimum) + 1) + minimum;
+//}
+
+
