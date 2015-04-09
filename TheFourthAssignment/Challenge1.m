e62133e85d9ceb1d64865fa152e6babb046c2b75
CraigStojsin <stojsin@outlook.com>
2015-04-08 22:26:36 -0400

    challenge 1

--- TheFourthAssignment/Challenge1.m
+++ TheFourthAssignment/Challenge1.m
@@ -6,12 +6,23 @@
 //  Copyright (c) 2015 Codes By Craig. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
+
+float newTemp(float fahrenheit){
+    float celsius = (fahrenheit - 32) / 1.8;
+    return celsius;
+}
+
+float fahrenheit = 27;
+
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSLog(@"%f Fahrenheit is %.2f celsius",fahrenheit,newTemp(fahrenheit)) ;
+        
     }
     return 0;
 }
